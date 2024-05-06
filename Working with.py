8424a31638ad3f33c6b03e2d51f4e8f02860ae01
Addo Bentil <addobentil100@gmail.com>
2024-05-06 22:28:14 +0000

    working with numeric data types exercise

---
+++ Working with.py
@@ -0,0 +1,30 @@
+# Adding
+# 2+2
+
+# Subtracting
+# 4-3
+
+# Exiting the Python shell
+# quit()
+
+print("Python has three numeric types: int, float, and complex")
+
+# Creating a variable, print value and type
+# myValue=1
+# print(myValue)
+# print(type(myValue))
+
+# Combining numbers with strings
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# the complex data type
+# myValue=5j
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# the bool data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
